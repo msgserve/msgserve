90bb9c5c076d8e5e32832fc08ea28376d61db845
Herbert Poul <herbert@codeux.design>
2025-11-15 08:17:07 +0100

    wip: add example for msgserve client.

--- packages/msgserve_client/lib/msgserve_client.dart
+++ packages/msgserve_client/lib/msgserve_client.dart
@@ -8,3 +8,5 @@ export 'package:msgserve_client/src/widget/msgserve_interstitial.dart'
 export 'package:msgserve_client/src/widget/msgserve_banner_carousel.dart'
     show MsgServeBannerCarousel;
 export 'package:msgserve_client/src/msgserve_event.dart';
+export 'package:msgserve_shared/msgserv_shared.dart';
+export 'package:openapi_base/openapi_base.dart' show ApiUuid;
